6ed45948951f1a49ec7ae33da28f18f0df7094c0
SADMAN-SADID-052 <sadman.cse8.bu@gmail.com>
2026-02-28 16:28:06 +0600

    convert obj data to json and json to obj

---
+++ jsonConvert.js
@@ -0,0 +1,23 @@
+const person = {
+
+    name:"Rahim",
+    age:24,
+    friends:["BD","Test"],
+    country:'Bangladesh',
+    family:{
+        father_name : "karim",
+        age:60,
+        mother_name : "Karima"
+
+
+        
+    },
+}
+
+//obj to json data
+const jsonConvert = JSON.stringify(person);
+console.log(jsonConvert);
+
+// json data to obj
+const plaintext = JSON.parse(jsonConvert);
+console.log(plaintext)
