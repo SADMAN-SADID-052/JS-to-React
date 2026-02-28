8d50e1a9af732d86cf8ccf23329e9c68b7952c83
SADMAN-SADID-052 <sadman.cse8.bu@gmail.com>
2026-02-28 16:14:30 +0600

    Perform array and object destructuring

---
+++ array-destructring.js
@@ -0,0 +1,14 @@
+// const fruits = ['Mango','Orange','Apple','Banana'];
+// const element1 = fruits[0];
+// const element2 = fruits[1];
+// const element3 = fruits[2];
+
+// console.log(element1,element3)
+//Array destructuring
+
+const fruits = ['Mango','Orange','Apple','Banana'];
+
+const  [element3,element2,element1,element0] = fruits
+console.log(element1,element3,element0)
+
+
---
+++ obj-des.js
@@ -0,0 +1,11 @@
+const person = {
+
+    name:"Hero Alom",
+    age:24,
+    friends:["BD","Test"],
+    country:'bNAGLADESH'
+}
+
+const {name,age,country,friends} = person;
+
+console.log(friends)
