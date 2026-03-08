2412bc8ee9093d2f10b92914c3b2b75039ecac68
SADMAN-SADID-052 <sadman.cse8.bu@gmail.com>
2026-03-08 23:24:33 +0600

    implemnt dot and bracket notation

---
+++ Module-8/notation.js
@@ -0,0 +1,21 @@
+const person = {
+    name : "Hero Alom",
+    age : 10,
+    friends:["Korim", "Josim"],
+    salary:100,
+    10:"Secret code"
+
+}
+// dot notation
+const access = person.name;
+console.log(access)
+
+//bracket notation
+const access1 = person["name"];
+console.log(access1)
+
+const access2 = person[10];
+console.log(access2)
+
+
+
